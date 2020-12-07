811746e5d1dd57c465a97395db8a14fabfa67639
Lea Droguet <ljdroguet@gmail.com>
2020-12-07 15:45:20 +0000

    update windows agent

--- agent/Windows/firewallrules.ps1
+++ agent/Windows/firewallrules.ps1
@@ -1,23 +1,22 @@
 # Plugin "Firewall Rules" OCSInventory
 # Author: Léa DROGUET
 
-$rules = Get-NetFirewallRule | Select-Object -Property DisplayName, Description, Enabled, 
-@{Name='Protocol';Expression={($PSItem | Get-NetFirewallPortFilter).Protocol}}, 
-@{Name='LocalPort';Expression={($PSItem | Get-NetFirewallPortFilter).LocalPort}}, 
-Direction, Action
+$ErrorActionPreference = 'silentlycontinue'
+$rules = Get-NetFirewallRule | Select-Object -Property DisplayName, Description, Action, Direction, Enabled, InstanceID
+$portsAndProtocols = Get-NetFirewallPortFilter | Select-Object -Property LocalPort, Protocol, InstanceID
 
-$xml += "<FIREWALLRULES>"
+$xml += "<FIREWALL_RULES>"
 foreach ($rule in $rules) {
+    $ruleDetails = $portsAndProtocols | Where-Object {$_.instanceID -eq $rule.instanceID}
     $xml += "`t<RULE>`n"
-    $xml += "`t`t<DISPLAYNAME>"+ $rule.DisplayName +"</DISLAYNAME>`n" 
+    $xml += "`t`t<DISPLAY_NAME>"+ $rule.DisplayName +"</DISLAYNAME>`n" 
     $xml += "`t`t<DESCRIPTION>"+ $rule.Description +"</DESCRIPTION>`n"
     $xml += "`t`t<ENABLED>"+ $rule.Enabled +"</ENABLED>`n"
     $xml += "`t`t<DIRECTION>"+ $rule.Direction +"</DIRECTION>`n"
     $xml += "`t`t<ACTION>"+ $rule.Action +"</ACTION>`n"
-    $xml += "`t`t<PORT>"+ $rule.LocalPort +"</PORT>`n"
-    $xml += "`t`t<PROTOCOL>"+ $rule.Protocol +"</PROTOCOL>`n"
+    $xml += "`t`t<PORT>"+ $ruleDetails.LocalPort +"</PORT>`n"
+    $xml += "`t`t<PROTOCOL>"+ $ruleDetails.Protocol +"</PROTOCOL>`n"
     $xml += "`t</RULE>`n"
 }
-
-$xml += "</FIREWALLRULES>"
+$xml += "</FIREWALL_RULES>"
 echo $xml
