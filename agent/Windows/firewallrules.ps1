d29f0db2a27cc81d7aa7881a7e91dfccfdbb425e
Lea Droguet <ljdroguet@gmail.com>
2020-12-09 14:38:29 +0000

    correct wrong tag and add encoding info for windows agent

--- agent/Windows/firewallrules.ps1
+++ agent/Windows/firewallrules.ps1
@@ -5,18 +5,18 @@ $ErrorActionPreference = 'silentlycontinue'
 $rules = Get-NetFirewallRule | Select-Object -Property DisplayName, Description, Action, Direction, Enabled, InstanceID
 $portsAndProtocols = Get-NetFirewallPortFilter | Select-Object -Property LocalPort, Protocol, InstanceID
 
-$xml += "<FIREWALL_RULES>"
 foreach ($rule in $rules) {
     $ruleDetails = $portsAndProtocols | Where-Object {$_.instanceID -eq $rule.instanceID}
-    $xml += "`t<RULE>`n"
-    $xml += "`t`t<DISPLAY_NAME>"+ $rule.DisplayName +"</DISLAYNAME>`n" 
-    $xml += "`t`t<DESCRIPTION>"+ $rule.Description +"</DESCRIPTION>`n"
-    $xml += "`t`t<ENABLED>"+ $rule.Enabled +"</ENABLED>`n"
-    $xml += "`t`t<DIRECTION>"+ $rule.Direction +"</DIRECTION>`n"
-    $xml += "`t`t<ACTION>"+ $rule.Action +"</ACTION>`n"
-    $xml += "`t`t<PORT>"+ $ruleDetails.LocalPort +"</PORT>`n"
-    $xml += "`t`t<PROTOCOL>"+ $ruleDetails.Protocol +"</PROTOCOL>`n"
-    $xml += "`t</RULE>`n"
+    $xml += "<FIREWALLRULES>`n"
+    $xml += "<DISPLAYNAME>"+ $rule.DisplayName +"</DISPLAYNAME>`n" 
+    $xml += "<DESCRIPTION>"+ $rule.Description +"</DESCRIPTION>`n"
+    $xml += "<ENABLED>"+ $rule.Enabled +"</ENABLED>`n"
+    $xml += "<DIRECTION>"+ $rule.Direction +"</DIRECTION>`n"
+    $xml += "<ACTION>"+ $rule.Action +"</ACTION>`n"
+    $xml += "<PORT>"+ $ruleDetails.LocalPort +"</PORT>`n"
+    $xml += "<PROTOCOL>"+ $ruleDetails.Protocol +"</PROTOCOL>`n"
+    $xml += "</FIREWALLRULES>`n"
 }
-$xml += "</FIREWALL_RULES>"
-echo $xml
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::WriteLine($xml)
